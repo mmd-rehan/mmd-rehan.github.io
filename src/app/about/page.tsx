82ddd56c8e8110e40118fc0fabc672a82aca4cab
Muhammad Rehan <mrmuhammadrehan65@gmail.com>
2025-05-29 19:03:31 +0400

    updated about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -27,16 +27,18 @@ const page = () => {
 
         <FramerWrapper y={0} x={100} >
           <p className=" font-poppins bg-white text-xl w-full text-primary max-sm:text-lg">
-            I am a software engineer working on web applications and mobile applications. I have been
-            developing professionally for 7 years but tinkering since a kid. I started in tech with internships, freelance services and
-            part time positions during college.
-            I have a bit off a diverse job history. I have been working on IOT’s, Mobile App Development, System Engineer,
-            graphic/web design, web app development and sysadmin/DevOps.
-            <br />I can help everywhere in the stack and I love wearing
-            multiple hats to an extent. Although, I do think my strongest skills are in software engineering.
-            Most of my positions utilized agile-like development processes using Jira.
-            A day in my life may consist of: prepping web art, fixing some front-end bugs, adding an API endpoint, dockerizing an
-            application, database design, configuring a library, setting up a build or deployment plan
+            Hay i am Rehan - the full-stack wizard who codes like a storm and solves problems like a detective with a debugger. Based in the heart of Dubai, im a seasoned software engineer with roots in Pakistan, a degree in Computer Science, and battle scars from real-world tech wars.
+            From taming CI/CD pipelines on Ubuntu with GitLab runners, to designing sleek Next.js frontends with Tailwind, to wrangling ZKTeco devices for attendance logs
+            Rehan does it all.
+            <br />
+            <br />
+            I led teams, mentored devs, architected apps, and even helped reshape EMR systems and insurance platforms across industries.
+            my journey weaves through aviation tech at Amadeus, hybrid mobile apps in Flutter, microservices in Node.js, and even SEO-smart websites aligned with digital marketing goals. I dont just code i crafts, whether it's scalable infrastructure or user-friendly UIs.
+            <br />
+            <br />
+            <i>
+              Outside the terminal? I am a gamer, a binge-watcher, and a man who doesn’t mind a tough challenge — especially if it means learning something new.
+            </i>
           </p>
         </FramerWrapper>
       </div>
--- src/components/Aboutfooter.tsx
+++ src/components/Aboutfooter.tsx
@@ -4,7 +4,7 @@ const Aboutfooter = () => {
 
 
     const items = [
-        {name:"Language", answer:"English, Urdu, Hindi", icon:<Languages className="h-11 w-11" />},
+        {name:"Language", answer:"English, Urdu, Hindi, Pashto, Turkish", icon:<Languages className="h-11 w-11" />},
         {name:"Nationality",answer:"Pakistani", icon:<Globe2   className="h-8 w-8" />},
         {name:"Gender",answer:"Male", icon:<Dna  className="h-8 w-8" />},
     ]
@@ -15,7 +15,7 @@ const Aboutfooter = () => {
         {
             items.map((val, indx) => {
               return(
-                <div className="p-1 w-fit relative" key={indx}>
+                <div className="p-1 w-fit relative bg-white rounded-lg" key={indx}>
                 <h1 className="gap-2 text-3xl font-poppins text-primary font-semibold relative flex icon_underline max-sm:text-2xl">{val.icon}{val.name}</h1>
                 <div className="flex gap-2 justify-center items-center flex-row text-xl text-primary pt-3 max-lg:justify-start">
                 <Circle className="h-3 w-3" /> {val.answer}
